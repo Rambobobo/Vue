b5f20d9e1038d3cf499ddf9ed47b606e07d85edd
Lanjirong <578904035@qq.com>
2023-03-09 17:29:17 +0800

    修改主页面

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -1,16 +1,23 @@
 <template>
-  <el-menu default-active="1-4-1"
-           class="el-menu-vertical-demo"
-           @open="handleOpen"
-           @close="handleClose"
-           :collapse="isCollapse">
+  <el-menu
+    default-active="1-4-1"
+    class="el-menu-vertical-demo"
+    @open="handleOpen"
+    @close="handleClose"
+    :collapse="isCollapse"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+    ><el-menu-item index="2">
+      <i class="el-icon-menu"></i>
+      <span slot="title">导航二</span>
+    </el-menu-item>
     <el-submenu index="1">
       <template slot="title">
         <i class="el-icon-location"></i>
         <span slot="title">导航一</span>
       </template>
       <el-menu-item-group>
-        <span slot="title">分组一</span>
         <el-menu-item index="1-1">选项1</el-menu-item>
         <el-menu-item index="1-2">选项2</el-menu-item>
       </el-menu-item-group>
@@ -22,19 +29,6 @@
         <el-menu-item index="1-4-1">选项1</el-menu-item>
       </el-submenu>
     </el-submenu>
-    <el-menu-item index="2">
-      <i class="el-icon-menu"></i>
-      <span slot="title">导航二</span>
-    </el-menu-item>
-    <el-menu-item index="3"
-                  disabled>
-      <i class="el-icon-document"></i>
-      <span slot="title">导航三</span>
-    </el-menu-item>
-    <el-menu-item index="4">
-      <i class="el-icon-setting"></i>
-      <span slot="title">导航四</span>
-    </el-menu-item>
   </el-menu>
 </template>
 
@@ -43,20 +37,23 @@
   width: 200px;
   min-height: 400px;
 }
+.el-menu {
+  height: 100vh;
+}
 </style>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      isCollapse: true,
+      isCollapse: false,
     };
   },
   methods: {
-    handleOpen (key, keyPath) {
+    handleOpen(key, keyPath) {
       console.log(key, keyPath);
     },
-    handleClose (key, keyPath) {
+    handleClose(key, keyPath) {
       console.log(key, keyPath);
     },
   },
