d2b1af24060aaf5b2887315a8f584bc3f3bcab03
Lanjirong <578904035@qq.com>
2023-03-13 17:01:55 +0800

    修改登录页面页面

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,12 +1,15 @@
 <template>
-  <el-form :model="form" :rules="rules">
+  <el-form label-width="70px" :inline="true" class="login-container" :model="form" :rules="rules">
         <h3 class="login_title">测试用例管理系统登录</h3>
-        <el-from-item label="用户名" prop="username">
+        <el-form-item label="用户名" prop="username">
             <el-input v-model="form.username" placeholder="请输入账号"></el-input>
-        </el-from-item>
-        <el-from-item label="密码" prop="password">
-             <el-input v-model="form.password" placeholder="请输入密码"></el-input>
-        </el-from-item>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+             <el-input type="password" v-model="form.password" placeholder="请输入密码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button style="margin-left:105px;margin-top:10px" type="primary">登录</el-button>
+        </el-form-item>
     </el-form>
 </template>
 <script>
@@ -21,9 +24,29 @@ export default {
         username: [
           { required: true, trigger: "blur", message: "请输入用户名" }
         ],
-        password: [{ required: true, trigger: "blur", mesage: "请输入密码" }]
+        password: [{ required: true, trigger: "blur", message: "请输入密码" }]
       }
     }
   }
 }
 </script>
+<style lang="less" scoped>
+.login-container{
+  width: 350px;
+  border: 1px solid #eaeaea;
+  margin: 180px auto;
+  padding: 35px 35px 15px 35px;
+  background-color: #fff;
+  border-radius: 15px;
+  box-shadow:0 0 25px #cac6ca;
+  box-sizing: border-box;
+  .el-input{
+    width: 198px;
+  }
+  .login_title{
+    text-align: center;
+    margin-bottom: 40px;
+    color: #505458;
+  }
+}
+</style>
